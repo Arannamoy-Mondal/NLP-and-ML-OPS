f6b68d168a63c69f80f9eaa59554c067e5e169eb
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-08-26 21:56:59 +0600

    upd:add Probability

---
+++ Probability/1_Intro.py
@@ -0,0 +1,3 @@
+'''Probability is a measure of the likelihood that a particular event will occur. It ranges from 0 (impossible) to 1 (certain). Probability is used to model uncertanity with data and make informed decisions based on that uncertainty.
+
+'''
