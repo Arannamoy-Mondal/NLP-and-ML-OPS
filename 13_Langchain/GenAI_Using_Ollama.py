2e7c0a33a40e23e709e64a13b8424db17c79bbe5
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-11-15 00:49:10 +0000

    add genAI using streamlit and ollama

---
+++ 13_Langchain/GenAI_Using_Ollama.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from langchain_community.llms.ollama import Ollama 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm=Ollama(model="phi4:latest")
+
+question=st.text_input("Enter your question:")
+prompt=ChatPromptTemplate([
+    f'{question}'
+])
+
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if question:
+   st.write(chain.invoke({"question":question}))
+
+
+"""run command: streamlit run filename"""
